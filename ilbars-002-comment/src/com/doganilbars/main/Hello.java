7ef78c5e36d3398f0e46972387ec58b5b237b2a6
hdilbars <hd.ilbars@hotmail.com>
2018-05-08 03:17:10 +0300

    comment

---
+++ ilbars-002-comment/src/com/doganilbars/main/Hello.java
@@ -0,0 +1,38 @@
+package com.doganilbars.main;
+
+public class Hello {
+
+	// Single line comment and single description line
+
+	// Tek açýklama satýrý
+
+	/*
+	 * Multiple Line comment and multiple description line
+	 * 
+	 */
+
+	/*
+	 * 
+	 * Çoklu açýklama satýrý
+	 * 
+	 * 
+	 */
+
+	public static void main(String[] args) {
+		// java compiler ignores the comment
+
+		// java derleyici komut satýrýný yok sayar yani programý etkilemez
+		System.out.println("Hello World");
+		/*
+		 * Comments make code easily understandable
+		 */
+
+		/*
+		 * Açýklama satýrlarýyla yapýlan yorumlar
+		 * 
+		 * kodu anlaþýlabilir yapar
+		 * 
+		 */
+	}
+
+}
