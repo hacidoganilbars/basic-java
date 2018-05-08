98c9429bfd63324759b855d0b216d0e0e5535c53
hdilbars <hd.ilbars@hotmail.com>
2018-05-08 03:19:52 +0300

    increment and decrement

---
+++ ilbars-006-incrementanddecrement/src/com/doganilbars/main/IncrementAndDecrement.java
@@ -0,0 +1,21 @@
+package com.doganilbars.main;
+
+public class IncrementAndDecrement {
+	public static void main(String[] args) {
+
+		int number1 = 10, number2 = 20;
+		int number3 = 5, number4 = 7;
+
+		System.out.println(number1++);
+		System.out.println(number1++);
+		System.out.println(number1);
+		System.out.println(++number2);
+		System.out.println(number2);
+		System.out.println(number3--);
+		System.out.println(number3);
+		System.out.println(--number4);
+		System.out.println(number4);
+
+	}
+
+}
