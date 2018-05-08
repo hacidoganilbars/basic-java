a784cd4ee67675a419d34a2c62519571f446c8fe
hdilbars <hd.ilbars@hotmail.com>
2018-05-08 03:18:02 +0300

    declaration

---
+++ ilbars-003-declaration/src/com/doganilbars/main/DeclareAndInitializeStringVariable.java
@@ -0,0 +1,11 @@
+package com.doganilbars.main;
+
+public class DeclareAndInitializeStringVariable {
+	public static void main(String[] args) {
+		String str1 = "My name is Hacý Doðan Ýlbars";
+		System.out.println("String is: " + str1);
+		String str2 = new String("Java Developer");
+		System.out.println("The Second string is: " + str2);
+	}
+
+}
---
+++ ilbars-003-declaration/src/com/doganilbars/main/DeclareAndIntializeIntegerVariable.java
@@ -0,0 +1,12 @@
+package com.doganilbars.main;
+
+public class DeclareAndIntializeIntegerVariable {
+
+	public static void main(String[] args) {
+		// int ->(integer)=tamsayı demektir
+		int number;
+		number = 10;
+		System.out.println("The value of number is (Sayının değeri): " + number);
+	}
+
+}
